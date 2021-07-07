cceed9603ab3eb80bfa15fc7da2e78450cfe6e64
Alejandro9917 <alejandromelendez99utlook.es>
2021-07-07 13:53:25 -0600

    Agregando nuevas vistas a la demo

---
+++ src/main/java/org/itm/web/ContactResource.java
@@ -0,0 +1,29 @@
+package org.itm.web;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import io.quarkus.qute.Template;
+import io.quarkus.qute.TemplateInstance;
+import io.quarkus.qute.api.ResourcePath;
+import io.quarkus.security.Authenticated;
+import io.quarkus.security.identity.SecurityIdentity;
+
+@Path("contact")
+@Authenticated
+public class ContactResource {
+    @Inject
+    SecurityIdentity securityIdentity;
+
+    @ResourcePath("contact")
+    Template contact;
+
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    public TemplateInstance noauth() {
+        return contact.data("username", securityIdentity.getPrincipal().getName());
+    }
+}
