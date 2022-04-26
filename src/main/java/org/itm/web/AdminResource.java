e50bdfe98099ceb3cab37a9ca404ccc50bb46f20
R-Lam <ricardolam3@gmail.com>
2022-04-25 18:27:51 -0600

    Upd to work with keycloak 17+ & java 17

--- src/main/java/org/itm/web/AdminResource.java
+++ src/main/java/org/itm/web/AdminResource.java
@@ -2,18 +2,12 @@ package org.itm.web;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 
-import io.quarkus.security.Authenticated;
-
-@Path("/admin")
-@Authenticated
+@Path("/api/admin")
 public class AdminResource {
 
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String admin() {
+    public String manage() {
         return "granted";
     }
 }
